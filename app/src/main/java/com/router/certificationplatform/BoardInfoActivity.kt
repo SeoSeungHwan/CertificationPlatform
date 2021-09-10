6377bb274b19532909da74a5c4942284c9515e90
DESKTOP-RE5PC2N\seunghwan <tmdghks3264@naver.com>
2021-09-11 00:00:41 +0900

    게시물 목록 UI 변경

---
+++ app/src/main/java/com/router/certificationplatform/BoardInfoActivity.kt
@@ -0,0 +1,11 @@
+package com.router.certificationplatform
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BoardInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_board_info)
+    }
+}
